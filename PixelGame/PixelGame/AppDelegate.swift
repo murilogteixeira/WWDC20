505e3acf576498e0731b84c90f91b19eb63f1a78
Murilo Teixeira <murilogteixeira@gmail.com>
2020-03-31 11:30:59 -0300

    Adicionar novo projeto para corrigir erro

---
+++ PixelGame/PixelGame/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  PixelGame
+//
+//  Created by Murilo Teixeira on 31/03/20.
+//  Copyright © 2020 Murilo Teixeira. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
